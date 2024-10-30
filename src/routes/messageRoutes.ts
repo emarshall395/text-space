6b81da58f3ae94325e4fb39e5511d2e9687f70c0
Kaiser Vanguardia <122261201+thegiankaiser@users.noreply.github.com>
2024-10-29 21:20:31 -0700

    Update messageRoutes.ts
    Basically need to match App.ts of Professor's code.

--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -1,5 +1,5 @@
 // src/routes/messageRoutes.ts
-import { Router } from 'express';
+import * as express from 'express';
 import {
   getAllMessages,
   createMessage,
@@ -9,15 +9,26 @@ import {
   getMessagesBySenderAndReceiver,
   getMessagesForReceiver
 } from '../controllers/messageController';
- 
-const router = Router();
- 
-router.get('/', getAllMessages); // Get all messages
-router.post('/', createMessage); // Create a new message
-router.get('/:id', getMessageById); // Get a specific message by ID
-router.put('/:id', updateMessage); // Update a message by ID
-router.delete('/:id', deleteMessage); // Delete a message by ID
-router.get('/sender/:senderID/receiver/:receiverID', getMessagesBySenderAndReceiver); // Get messages by sender and receiver
-router.get('/receiver/:receiverID', getMessagesForReceiver); // Get messages for a specific receiver
- 
-export default router;
+
+class MessageRoutes {
+  public router: express.Router;
+
+  constructor() {
+    this.router = express.Router();
+    this.routes();
+  }
+
+  // Define API endpoints
+  private routes(): void {
+    this.router.get('/', getAllMessages); // Get all messages
+    this.router.post('/', createMessage); // Create a new message
+    this.router.get('/:id', getMessageById); // Get a specific message by ID
+    this.router.put('/:id', updateMessage); // Update a message by ID
+    this.router.delete('/:id', deleteMessage); // Delete a message by ID
+    this.router.get('/sender/:senderID/receiver/:receiverID', getMessagesBySenderAndReceiver); // Get messages by sender and receiver
+    this.router.get('/receiver/:receiverID', getMessagesForReceiver); // Get messages for a specific receiver
+  }
+}
+
+const messageRoutes = new MessageRoutes();
+export default messageRoutes.router;
