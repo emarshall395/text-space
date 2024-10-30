d377133e0ad94fa43bb2c7c281fe633e5cad1bc8
himaja <61680051+himaja0007@users.noreply.github.com>
2024-10-29 21:59:38 -0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,14 @@
-// src/server.ts
 import express from 'express';
 import mongoose from 'mongoose';
-import messageRoutes from './routes/messageRoutes';
- 
-const app = express();
+import messageRoutes from './routes/messageRoutes'; 
+import { App } from './App';
+
 const PORT = process.env.PORT || 5000;
- 
-// Middleware
-app.use(express.json());
- 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/your_db_name', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
- 
-// Use message routes
+const app = new App().express;
+
+
 app.use('/api/messages', messageRoutes);
- 
-// Error handling middleware
-app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
- 
-// Start the server
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
