c3015d373148aed6d0c2ca6c6b33de23e6b0b5e3
Kaiser Vanguardia <122261201+thegiankaiser@users.noreply.github.com>
2024-10-29 23:25:03 -0700

    Update Message.ts

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -1,19 +1,23 @@
 // src/models/Message.ts
 import mongoose, { Document, Schema } from 'mongoose';
- 
+
+// IMessage interface that extends Document from Mongoose
 export interface IMessage extends Document {
   senderID: string;
   receiverID: string;
+  messageID: string;
   content: string;
   createdAt: Date;
 }
- 
+
+// Define the message schema
 const messageSchema: Schema = new Schema({
   senderID: { type: String, required: true },
   receiverID: { type: String, required: true },
-  messageID: { type: String, required: true },
+  messageID: { type: String, required: true }, // 
   content: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now }, // 
 });
- 
+
+// Export the model
 export default mongoose.model<IMessage>('Message', messageSchema);
