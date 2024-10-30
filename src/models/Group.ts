8c303539bb521080ab8f0d9b2b3b9e7856f7fe94
emarshall395 <49788959+emarshall395@users.noreply.github.com>
2024-10-29 22:50:50 -0700

    Update Group.ts

--- src/models/Group.ts
+++ src/models/Group.ts
@@ -1,1 +1,18 @@
-
+// src/models/Group.ts
+import mongoose, { Document, Schema } from 'mongoose';
+ 
+export interface IGroup extends Document {
+  senderID: string;
+  chatMembers: [string];
+  content: [string];
+  createdAt: Date;
+}
+ 
+const groupSchema: Schema = new Schema({
+  senderID: { type: String, required: true },
+  chatMembers: { type: [String], required: true },
+  content: { type: [String], required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+ 
+export default mongoose.model<IGroup>('Group', groupSchema);
