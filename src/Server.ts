5102eaf4c8dcc6ac0e60f027de3b18f027eaabbf
Kaiser Vanguardia <122261201+thegiankaiser@users.noreply.github.com>
2024-10-31 00:56:08 -0700

    Update Server.ts

--- src/Server.ts
+++ src/Server.ts
@@ -1,27 +1,33 @@
-
 // src/server.ts
 import messageApp from './routes/messageRoutes';
 import * as dotenv from 'dotenv';
 import connectDB from './dbConnect';
 import express from 'express';
-import app from './App';
+
+dotenv.config();
 
 const serverApp = express();
+const PORT = process.env.PORT || 5025;
 
 // Middleware
 serverApp.use(express.json());
 
-// Register routes
-app.use('/api/messages', messageApp);
-
-dotenv.config();
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+
+// Register routes
+serverApp.use('/api/messages', messageApp);
+
+
+// Define a route for the root path
+serverApp.get('/', (req, res) => {
+  res.send('Welcome to the Messages API! Use /api/messages to get messages.');
+});
 
 // Start the server
-app.listen(PORT, () => {
+serverApp.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-export default app;
+export default serverApp;
+
